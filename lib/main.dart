f233b8f5c3d61ae76c4eeffe77f2695d95f9846e
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2024-12-01 12:34:16 +0900

    10対応

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _apiUrl = "https://narutodb.xyz/api/character";
+  List<Character> _characters = [];
 
   @override
   void initState() {
@@ -37,19 +38,69 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _fetchCharacters() async {
     final response = await Dio().get(_apiUrl);
-    final List<dynamic> data = response.data["Characters"];
-    final characters = data.map((data) => Character.fromJson(data)).toList();
-    print(characters);
+    final List<dynamic> data = response.data["characters"];
+
+    setState(() {
+      _characters = data.map((data) => Character.fromJson(data)).toList();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Color(0xFFBCE2E8),
         title: const Text("NARUTO図鑑"),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView.builder(
+          itemCount: _characters.length,
+          itemBuilder: (context, index) {
+            final character = _characters[index];
+            return Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: character.images.isNotEmpty
+                        ? Image.network(
+                            character.images[0],
+                            fit: BoxFit.contain,
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Image(
+                                  image: AssetImage("assets/dummy.png"));
+                            },
+                          )
+                        : const Image(
+                            image: AssetImage("assets/dummy.png"),
+                          ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      "テストキャラクター",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, bottom: 8),
+                    child: Text(
+                      "なし",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
